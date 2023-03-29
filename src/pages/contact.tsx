0e9940ca4402dd0cfc06ebfbae067bd8857b2886
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 14:10:43 +0800

    add contact section : contact layout config

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,22 @@
+import Copyrights from '@/components/shared/Copyrights'
+import Playgrounds from '@/components/shared/Playgrounds'
+import Socials from '@/components/shared/Socials'
 import React from 'react'
 
 export default function contact() {
   return (
-    <div>contact</div>
+    <div className='fixed bottom-[6rem] right-0 p-[2rem] uppercase sm:top-[15rem] sm:right-[15rem]'>
+      <div className='flex flex-col gap-6'>
+        <div>
+          <p  className='mb-2 font-bold'>Socials</p>
+          <Socials />
+        </div>
+        <div>
+          <p  className='mb-2 font-bold'>Playgrounds</p>
+          <Playgrounds />
+        </div>
+      </div>
+      <Copyrights />
+    </div>
   )
 }
