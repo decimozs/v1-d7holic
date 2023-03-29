34fa4f1e4cee6b5be36d0d297ff17e751908a773
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 21:23:38 +0800

    remove progress bar : fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
 import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
-import '@/styles/Nprogress.css'
 import type { AppProps } from 'next/app'
-import Router from 'next/router'
-import NProgress from 'nprogress'; 
-
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
-Router.events.on('routeChangeError', () => NProgress.done());
-NProgress.configure({ showSpinner: false })
-NProgress.configure({ easing: 'ease-in', speed: 700 });
-NProgress.configure({ trickleSpeed: 900 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
