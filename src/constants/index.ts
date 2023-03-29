0b85bc0c976e4c828a65fdfb766868155c2c3741
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 15:12:47 +0800

    add loading bar : animate config

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -19,17 +19,17 @@ export const socialLinks : Array<SocialLinks> = [
     {
         title : "Instagram",
         link : "/",
-        hover: "hover:text-gray-500 transition-all"
+        hover: "hover:text-pink-500 hover:font-bold transition-all hover:underline-offset-4 hover:underline"
     },
     {
         title : "Github",
         link : "/",
-        hover: "hover:text-gray-500 transition-all"
+        hover: "hover:text-black hover:font-bold transition-all hover:underline-offset-4 hover:underline"
     },
     {
         title : "LinkedIn",
         link : "/",
-        hover: "hover:text-gray-500 transition-all"
+        hover: "hover:text-blue-600 hover:font-bold transition-all hover:underline-offset-4 hover:underline"
     }
 ]
 
@@ -37,11 +37,11 @@ export const playgroundLinks : Array<PlaygroundLinks> = [
     {
         title : "Leetcode",
         link : "/",
-        hover: "hover:text-gray-500 transition-all"
+        hover: "hover:text-yellow-500 hover:font-bold transition-all hover:underline-offset-4 hover:underline"
     },
     {
         title : "HackerRank",
         link : "/",
-        hover: "hover:text-gray-500 transition-all"
+        hover: "hover:text-green-500 hover:font-bold transition-all hover:underline-offset-4 hover:underline"
     },
 ]
