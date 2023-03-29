a01cc03f926868874b6dbdb294df4179d90080a4
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 22:34:43 +0800

    contact context : new add

---
+++ src/components/shared/ContactContext.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ContactContext() {
+  return (
+    <div className='p-[2rem] flex flex-col font-bold uppercase'>
+      <a href="">Email</a>
+      <p className='fixed bottom-0 right-0 p-[2rem]'>Design and Develop by Marlon Martin</p>
+      <a href='/' className='text-right'>Contact Form</a>
+    </div>
+  )
+}
