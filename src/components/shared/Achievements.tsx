f8c1bf4f08c7c8643dd7287ae60d5f61d3eea559
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 22:34:18 +0800

    achivements components : new add

---
+++ src/components/shared/Achievements.tsx
@@ -0,0 +1,17 @@
+import { achievementsLinks } from '@/constants'
+import Link from 'next/link'
+import React from 'react'
+
+export default function () {
+  return (
+    <div>
+        <ul>
+            {achievementsLinks.map((item, index) => (
+                <li key={index} className={`${item.hover}`}>
+                    <Link href={`${item.link}`} >{item.title}</Link>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
