f77a2e68c20fdad97c45bd89c39bf2c51f659266
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 22:42:17 +0800

    link navigations : fixed

--- src/components/shared/ContactContext.tsx
+++ src/components/shared/ContactContext.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function ContactContext() {
   return (
     <div className='p-[2rem] flex flex-col font-bold uppercase'>
-      <a href="">Email</a>
+      <Link href="/">Email</Link>
       <p className='fixed bottom-0 right-0 p-[2rem]'>Design and Develop by Marlon Martin</p>
-      <a href='/' className='text-right'>Contact Form</a>
+      <Link href='/' className='text-right'>Contact Form</Link>
     </div>
   )
 }
