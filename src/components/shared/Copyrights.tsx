6869685a665355c709505c87be93d0d99e4a920c
Decimo <marlonadiguemartin548@gmail.com>
2023-03-29 22:34:54 +0800

    copyrights components : new add

--- src/components/shared/Copyrights.tsx
+++ src/components/shared/Copyrights.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Copyrights() {
   return (
     <div className='fixed bottom-0 right-0 p-[2rem] font-bold'>
-        <h1>Design & Develop by Marlon Martin</h1>
+        <h1>Design and Built by Marlon Martin.</h1>
     </div>
   )
 }
