cce9cd0d7aab2de0472bf524ca94adbe0ecce1e8
Decimo <marlonadiguemartin548@gmail.com>
2023-03-30 13:09:41 +0800

    update : update the picture component

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,7 +1,9 @@
 import me from './m.jpg'
 import m1 from './m1.jpeg'
+import m2 from './landscape.webp'
 
 export {
     me,
-    m1
+    m1,
+    m2
 }
--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { m1, me } from '../../../public/images'
+import { m1, me, m2 } from '../../../public/images'
 
 export default function Picture() {
   return (
       <div className='fixed bottom-[4.5rem] right-0 pl-[2rem]'>
-        <Image src={m1} alt="pogi"></Image>
+        <Image src={m2} alt="pogi"></Image>
       </div>
   )
 }
