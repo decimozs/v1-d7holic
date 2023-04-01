a552f90f94dd309eaa1f711584076d40d0635b90
Decimo <marlonadiguemartin548@gmail.com>
2023-04-01 18:57:06 +0800

    update : improve responsiveness for large screens

--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -4,7 +4,7 @@ import { m2 } from '../../../public/images'
 
 export default function Picture() {
   return (
-      <div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[100%]'>
+      <div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
         <Image src={m2} alt="pogi" priority={true} ></Image>
       </div>
   )
