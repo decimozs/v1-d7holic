fbb4b6fb90f2c6f82186c28e5ba9380df94a7e76
Decimo <marlonadiguemartin548@gmail.com>
2023-04-01 19:09:21 +0800

    update : improve reponsiveness for tablets

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 export default function about() {
   return (
-    <div className='fixed bottom-0 left-0 p-[2rem] uppercase sm:top-[9%] sm:left-[50%]'>
+    <div className='fixed bottom-0 left-0 p-[2rem] uppercase sm:top-[9%] sm:left-[35%] lg:left-[50%]'>
       <div className='flex flex-col gap-[2rem] sm:flex-row'>
         <div className='w-full max-w-[300px]'>
             <p>Marlon Martin is an information technology student based in Manila, Philippines. Currently into <span className='font-bold'>Competitive programming</span> and <span className='font-bold'>Software Engineering</span>.</p>
