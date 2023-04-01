4cba6be00110bf03ba996c343e84583efdb94633
Decimo <marlonadiguemartin548@gmail.com>
2023-04-01 18:18:58 +0800

    update : fix rendering of image

--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -5,7 +5,7 @@ import { m1, me, m2 } from '../../../public/images'
 export default function Picture() {
   return (
       <div className='fixed bottom-[4.5rem] right-0 pl-[2rem]'>
-        <Image src={m2} alt="pogi"></Image>
+        <Image src={m2} alt="pogi" priority={true}></Image>
       </div>
   )
 }
