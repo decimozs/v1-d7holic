28830f66f2e17d5d3c3d5de875d65c2b3fb58382
Decimo <marlonadiguemartin548@gmail.com>
2023-04-08 14:01:33 +0800

    update : refactor codes

--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
 import { m2 } from '../../../public/images';
 
 const Picture = (): JSX.Element => {
   return (
-    <motion.div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
+    <div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
       <Image src={m2} alt="pogi" loading="lazy" />
-    </motion.div>
+    </div>
   );
 };
 
