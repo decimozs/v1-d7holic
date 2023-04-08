b8b73fda9b304e26c7f33880eb1df9ba43b5346e
Decimo <marlonadiguemartin548@gmail.com>
2023-04-08 14:01:03 +0800

    update : improve image optimization

--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -6,7 +6,7 @@ import { m2 } from '../../../public/images';
 const Picture = (): JSX.Element => {
   return (
     <motion.div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
-      <Image src={m2} alt="pogi" priority={true} loading="lazy" />
+      <Image src={m2} alt="pogi" loading="lazy" />
     </motion.div>
   );
 };
