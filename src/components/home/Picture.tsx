6d50679e238041770aae0fefcda792101b10bbfd
Decimo <marlonadiguemartin548@gmail.com>
2023-04-08 12:29:33 +0800

    fix : fix animations

--- src/components/home/Picture.tsx
+++ src/components/home/Picture.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import { m2 } from '../../../public/images'
+import { motion } from 'framer-motion'
 
 export default function Picture() {
   return (
-      <div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
+      <motion.div className='fixed bottom-[4.5rem] right-0 pl-[2rem] lg:max-w-[900px] xl:max-w-[80%]'>
         <Image src={m2} alt="pogi" priority={true} ></Image>
-      </div>
+      </motion.div>
   )
 }
