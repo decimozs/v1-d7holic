b10e3bcc892ffa62c60c490f588ea8e0fce721e2
Decimo <marlonadiguemartin548@gmail.com>
2023-04-08 13:09:54 +0800

    update : refactor codes

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,13 @@
 import ContactContext from '@/components/shared/ContactContext'
-import Copyrights from '@/components/shared/Copyrights'
-import Playgrounds from '@/components/shared/Playgrounds'
-import Socials from '@/components/shared/Socials'
+import { NextPage } from 'next';
 import React from 'react'
 
-export default function contact() {
+const contact : NextPage = () => {
   return (
     <div className='flex items-center justify-center h-screen'>
       <ContactContext />
     </div>
   )
 }
+
+export default contact;
